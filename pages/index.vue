dacf86f4dd57349d17f1c1584dd73985f00a0787
dalenysivilay <dalenysivilay@gmail.com>
2021-12-09 11:49:57 -0600

    Basic blog setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <section class="page-title">
+      <h1>Nuxt JAMStack Blog</h1>
+    </section>
+    <section class="blog-posts">
+      <h2>Latest Articles</h2>
+      <div v-for="post in posts" :key="post._id">
+        <h3><a v-bind:href="post.slug.current" v-text="post.title" /></h3>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
-export default {}
+import { groq } from '@nuxtjs/sanity'
+
+export default {
+  async asyncData({ $sanity }) {
+    const query = groq`*[_type == "post"]`
+    const posts = await $sanity.fetch(query)
+    return { posts }
+  }
+}
 </script>
+
+<style>
+  .main {
+    margin: 2rem;
+    min-height: 100vh;
+  }
+  .blog-posts {
+    margin: 2rem 0;
+  }
+</style>

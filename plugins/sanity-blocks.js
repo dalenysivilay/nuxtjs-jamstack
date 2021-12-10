f38deb9042f2673f2df0f5acd2a4b9a6de7246bc
dalenysivilay <dalenysivilay@gmail.com>
2021-12-10 09:14:03 -0600

    whatever

--- plugins/sanity-blocks.js
+++ plugins/sanity-blocks.js
@@ -1,3 +1,3 @@
 import Vue from "vue";
-import { SanityBlocks } from 'sanity-blocks-vue-component';
-Vue.component("block-content", SanityBlocks);
+import { BlockContent } from 'sanity-blocks-vue-component';
+Vue.component("block-content", BlockContent);
